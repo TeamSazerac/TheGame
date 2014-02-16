014f258b96dcb18e5fdc6a271541978f2bda37e0
Siko91 <lokikko@mail.bg>
2014-02-16 19:31:37 +0200

    Create Human.cs

---
+++ GameObjects/Human.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SazaracsMagicSword.GameObjects
+{
+    public class Human
+    {
+        public string Name { get; set; }
+    }
+}

62374837c419d2b9cea3350a8683311eff3d8f5a
Siko91 <lokikko@mail.bg>
2014-02-16 19:41:02 +0200

    Create Main.cs

---
+++ Main.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SazaracsMagicSword.GameObjects;
+using SazaracsMagicSword.Data;
+
+namespace SazaracsMagicSword
+{
+    class Start
+    {
+        static void Main(string[] args)
+        {
+            // --- MyPlan
+            //1) Main menu (new game / load game)
+            //2) Dynamic game
+            //3) In-game menu
+            //4) Conversations
+            //5) Battles
+            //6) Levels
+            //7) Tests
+        }
+    }
+}
